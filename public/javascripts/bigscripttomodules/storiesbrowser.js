13be9016e20909ff615ac1888bd004d6927cdb18
KalleJohansson <snabbakort@gmail.com>
2014-09-13 05:36:16 +0200

    jslintat

--- public/javascripts/bigscripttomodules/storiesbrowser.js
+++ public/javascripts/bigscripttomodules/storiesbrowser.js
@@ -1,41 +1,35 @@
-define('storiesbrowser', ['jquery'], function() {
-	 'use strict';
+define('storiesbrowser', ['jquery'], function () {
+    'use strict';
      //variables for stories browser
-     var ourlist = $('#ourlist').text(),
-     	jsonOur = $.parseJSON(ourlist),
-     	length = jsonOur.length, 
-     	pos = parseInt($('#ourpos').text(), 10),
-     	placeid = parseInt($('#ourPlaceId').text()),
-     	prev = $('.simplePrevious'),
-     	next = $('.simpleNext');
-     
-     //check if we have prev story
-     function hasPrev () {
-         if ($(prev).hasClass('simpleActive')) {
-             return true;
-         } else {
-             return false;
-         }
-     }
-     
-     //check if we have next story
-     function hasNext () {
-         if ($(next).hasClass('simpleActive')) {
-             return true;
-         } else {
-             return false;
-         }
-     }
-     
-     //show the story selected by the stories-browser
-     function showItem (pos) {
-         var id = jsonOur[pos];
-         
-         $('#showOrEditStorySection')
-         .load(routes.showStory({storyid: id, placeid: placeid, fromplace: 'false'}) + ' #showOrEditStorySection', function () {
+    var ourlist = $('#ourlist').text(),
+        jsonOur = $.parseJSON(ourlist),
+        length = jsonOur.length,
+        pos = parseInt($('#ourpos').text(), 10),
+        placeid = parseInt($('#ourPlaceId').text(), 10),
+        prev = $('.simplePrevious'),
+        next = $('.simpleNext');
+    //check if we have prev story
+    function hasPrev() {
+        if ($(prev).hasClass('simpleActive')) {
+            return true;
+        }
+        return false;
+    }
+    //check if we have next story
+    function hasNext() {
+        if ($(next).hasClass('simpleActive')) {
+            return true;
+        }
+        return false;
+    }     
+    //show the story selected by the stories-browser
+    function showItem(pos) {
+        var id = jsonOur[pos];
+       $('#showOrEditStorySection')
+            .load(routes.showStory({storyid: id, placeid: placeid, fromplace: 'false'}) + ' #showOrEditStorySection', function () {
 //             PlatsR_GUI.initStoriesBrowser();
 //             PlatsR_GUI.initDeleteStory();
-         });
+            });
      }
      
      $(prev + ' a').click(function (e) {
